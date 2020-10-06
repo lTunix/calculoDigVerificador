186daec46334dcddbe2543901ca24b6feb9aaa34
Marcos Fernández <marco.19.96@hotmail.com>
2020-10-06 18:00:45 -0300

    Actualiza mensajes de Error y diseño

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
 <!-- Por Marcos Fernandez -->
 <html lang="es">
-<<<<<<< HEAD
-
-=======
->>>>>>> 977dfd5b4de01076ac7ead6861633d4824d1faad
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cálculo Digito Verificador Rut</title>
 </head>
-<<<<<<< HEAD
 
 <body>
     <style type="text/css">
@@ -95,67 +90,6 @@
 
         var param = {
             rut: document.getElementById("rut").value
-=======
-<body>
-<style type="text/css">
-  .boton_personalizado{
-    text-decoration: none;
-    padding: 10px;
-    font-weight: 600;
-    font-size: 15px;
-    color: #ffffff;
-    background-color: #1883ba;
-    border-radius: 6px;
-    border: 2px solid #0016b0;
-  }
-
-  .contenido {
-  background-color: #fafafa;
-  margin: 1rem;
-  margin-top: 15%;
-  padding: 1rem;
-  border: 2px solid #ccc;
-  /* IMPORTANTE */
-  text-align: center;
-}
-
-.titulo {
-    font-size: 50px;
-    font-weight: bold;
-    font-family: 'Courier New', Courier, monospace;
-}
-
-.verificador {
-    width: 7px;
-    border: 0.5px solid red;
-    border-radius: 4px;
-    font-weight: bold;
-}
-</style>
-
-<div class="contenido">
-    <label class="titulo">Verifica tu RUT</label>
-    <br><br><br>
-    <label><b>Inserte el rut: <b/></label>
-    <input type="text" id="rut">
-    <label>-</label>
-    <input class="verificador" type="text" id="verificador" width="1%" disabled>
-    <br><br>
-    <input class="boton_personalizado" type="submit" name="" value="Calcular Dígito Verificador" id="calcular" onclick = "digitoVer();">
-</div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script>
-    function digitoVer(){
-        if(document.getElementById('rut').value == ""){
-            alert("No puede quedar el campo vacío.");
-            document.getElementById('verificador').focus();
-            return false;
-        }
-
-
-        var param = {
-        rut: document.getElementById("rut").value        
->>>>>>> 977dfd5b4de01076ac7ead6861633d4824d1faad
         };
 
         $.ajax({
@@ -167,7 +101,6 @@
             }
         });
     }
-<<<<<<< HEAD
 
     function setFormato() {
         document.getElementById('rut').value = document.getElementById('rut').value.replace(/[.]/g, '');
@@ -187,8 +120,3 @@
         }
     }
 </script>
-=======
-    </script>
-</body>
-</html>
->>>>>>> 977dfd5b4de01076ac7ead6861633d4824d1faad
